355074fc73507dc0589895987f080f1402f419a6
varun-naga <varunmulugu2000@gmail.com>
2022-03-25 11:35:02 +0530

    implemented CRUD operations

---
+++ src/main/java/com/example/demo/Topic.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class Topic {
+    private String id;
+    private String name;
+    private String description;
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Topic() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+}
